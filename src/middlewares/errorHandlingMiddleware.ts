ff5a0608a389a6f9da0bf5d55952214f2c3fcade
andreseichi <andre_seichi@hotmail.com>
2022-09-02 21:42:31 -0300

    middleware errorHandler

---
+++ src/middlewares/errorHandlingMiddleware.ts
@@ -0,0 +1,41 @@
+import { NextFunction, Request, Response } from "express";
+import { errors } from "../types/errors";
+
+export const errorHandlingMiddleware = (
+  error: errors,
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  if (error.type === "invalid_api_key")
+    return res.status(401).send({ message: error.message });
+
+  if (error.type === "invalid_employee_id")
+    return res.status(404).send({ message: error.message });
+
+  if (error.type === "card_already_exists")
+    return res.status(409).send({ message: error.message });
+
+  if (error.type === "invalid_card")
+    return res.status(404).send({ message: error.message });
+
+  if (error.type === "card_expired")
+    return res.status(401).send({ message: error.message });
+
+  if (error.type === "card_already_activated")
+    return res.status(409).send({ message: error.message });
+
+  if (error.type === "invalid_cvc")
+    return res.status(401).send({ message: error.message });
+
+  if (error.type === "invalid_password")
+    return res.status(401).send({ message: error.message });
+
+  if (error.type === "password_not_match")
+    return res.status(401).send({ message: error.message });
+
+  if (error.type === "card_already_blocked")
+    return res.status(409).send({ message: error.message });
+
+  return res.sendStatus(500);
+};
