ad709f8d1319a1ddbb3e1bf42f0388e460bdbdc0
tashiro <andre_seichi@hotmail.com>
2022-09-05 16:01:46 -0300

    port env or 4000

--- src/app.ts
+++ src/app.ts
@@ -15,7 +15,7 @@ app.use(cors());
 app.use(router);
 app.use(errorHandlingMiddleware);
 
-const { PORT } = process.env;
+const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
   console.log("server running on PORT " + PORT);
 });
